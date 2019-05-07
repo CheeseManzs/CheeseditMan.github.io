465794d8d217b1dd211971156dd33269d61cf55f
Cheeseit <36460677+CheeseditMan@users.noreply.github.com>
2019-05-07 17:58:51 -0400

    Update Reactor.js

--- Reactor.js
+++ Reactor.js
@@ -1,7 +1,7 @@
 Reactor_Heat = 100;
 Reactor_Power = 100;
 
-document.getElementById("demo").innerHTML = Reactor_Power;
+document.getElementById("Status").innerHTML = Reactor_Power;
 
 while(Reactor_Heat > 0){
 Reactor_Power++;
