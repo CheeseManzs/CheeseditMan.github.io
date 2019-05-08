be6b78325b32d1c50406bd1166966cbd81fd7a79
Cheeseit <36460677+CheeseditMan@users.noreply.github.com>
2019-05-08 16:13:36 -0400

    Update Reactor.js

--- Uranius/Reactor.js
+++ Uranius/Reactor.js
@@ -1,10 +1,13 @@
-Reactor_Heat = 100;
-Reactor_Power = 100;
+Reactor_Heat = 0;
+Reactor_Power = 5000;
 
 document.getElementById("Status").innerHTML = 'Power:'+ Reactor_Power;
 document.getElementById("Status-Heat").innerHTML = 'Heat: '+ Reactor_Heat;
 
 while(Reactor_Heat > 0){
+setTimeout(convert(), 1000)
+}
+function convert(){
 Reactor_Power++;
 Reactor_Heat = Reactor_Heat - 1;
 document.getElementById("Status").innerHTML = 'Power:'+ Reactor_Power;
