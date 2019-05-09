060f9355c949045dee4310279f74ae9d4ffa367b
Cheeseit <36460677+CheeseditMan@users.noreply.github.com>
2019-05-09 16:03:58 -0400

    Update Reactor.js

--- Uranius/Reactor.js
+++ Uranius/Reactor.js
@@ -5,7 +5,7 @@ var Power_Cells = 1;
 
 document.getElementById("Money").innerHTML = 'Money: $'+ Money;
 document.getElementById("Status").innerHTML = 'Power:'+ Reactor_Power;
-document.getElementById("Status-Cell").innerHTML = 'Power Cells:'+ Power_Cells;
+document.getElementById("Status-Cell").innerHTML = 'Power Cells:'+ Power_Cells + ' ($'+50*Power_cells+' for next order)';
 document.getElementById("Status-Heat").innerHTML = 'Heat: '+ Reactor_Heat/10 + ' C';
 
 convert = function convert(){
@@ -14,7 +14,7 @@ Reactor_Power = Reactor_Power + (1*Power_Cells);
 Reactor_Heat = Reactor_Heat + Math.round(1/Power_Cells);
 document.getElementById("Money").innerHTML = 'Money: $'+ Money;
 document.getElementById("Status").innerHTML = 'Power:'+ Reactor_Power;
-document.getElementById("Status-Cell").innerHTML = 'Power Cells:'+ Power_Cells;
+document.getElementById("Status-Cell").innerHTML = 'Power Cells:'+ Power_Cells + ' ($'+50*Power_cells+' for next order)';
 document.getElementById("Status-Heat").innerHTML = 'Heat: '+ Reactor_Heat/10 + ' C';
   
 console.log(Reactor_Power)
@@ -25,16 +25,18 @@ Reactor_Power = 0
 Reactor_Heat = Math.round(Reactor_Heat/2)
 document.getElementById("Money").innerHTML = 'Money: $'+ Money;
 document.getElementById("Status").innerHTML = 'Power:'+ Reactor_Power;
-document.getElementById("Status-Cell").innerHTML = 'Power Cells:'+ Power_Cells;
+document.getElementById("Status-Cell").innerHTML = 'Power Cells:'+ Power_Cells + ' ($'+50*Power_cells+' for next order)';
 document.getElementById("Status-Heat").innerHTML = 'Heat: '+ Reactor_Heat/10 + ' C';
 }
 cell = function cell(){
 
+if(Money >= 50*Power_Cells){
   Money = Money - 50;
   Power_Cells = Power_Cells + 1;
+}
 document.getElementById("Money").innerHTML = 'Money: $'+ Money;
 document.getElementById("Status").innerHTML = 'Power:'+ Reactor_Power;
-document.getElementById("Status-Cell").innerHTML = 'Power Cells:'+ Power_Cells;
+document.getElementById("Status-Cell").innerHTML = 'Power Cells:'+ Power_Cells + ' ($'+50*Power_cells+' for next order)';
 document.getElementById("Status-Heat").innerHTML = 'Heat: '+ Reactor_Heat/10 + ' C';
 
 }
