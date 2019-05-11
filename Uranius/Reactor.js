b8236bc74effc770368ab9a5a6c73d4c8e1cc724
Cheeseit <36460677+CheeseditMan@users.noreply.github.com>
2019-05-11 09:53:16 -0400

    Update Reactor.js

--- Uranius/Reactor.js
+++ Uranius/Reactor.js
@@ -4,6 +4,7 @@ var Reactor_Power = 0;
 var Money = 100;
 var Power_Cells = 1;
 var Reactor_Towers = 0;
+var Reactor_Clusters = 0;
 
 
 
@@ -13,12 +14,14 @@ document.getElementById("Status-Cell").innerHTML = 'Power Cells:'+ Power_Cells;
 document.getElementById("Status-Heat").innerHTML = 'Heat: '+ Reactor_Heat/10 + ' C';
 document.getElementById("Price").innerHTML = 'Price for next cell: $'+ 50*Power_Cells;
 document.getElementById("Tower").innerHTML = 'Reactor Towers: '+ Reactor_Towers;
+document.getElementById("Cluster").innerHTML = 'Reactors: '+ Reactor_Clusters;
 setInterval(function(){autoconvert()}, 100);
 convert = function convert(){
 
 Reactor_Power = Reactor_Power + (1*Power_Cells);
 Reactor_Heat = Reactor_Heat + Math.round(Power_Cells/2);
 Power_Cells = Power_Cells + (Reactor_Towers*20)
+Reactor_Towers = Reactor_Towers + (Reactor_Clusters*10)
 // Overheat:
 if(Reactor_Heat > Power_Cells*100){
 Money = Money/2
@@ -29,6 +32,7 @@ document.getElementById("Status-Cell").innerHTML = 'Power Cells:'+ Power_Cells;
 document.getElementById("Status-Heat").innerHTML = 'Heat: '+ Reactor_Heat/10 + ' C';
 document.getElementById("Price").innerHTML = 'Price for next cell: $'+ 50*Power_Cells;
 document.getElementById("Tower").innerHTML = 'Reactor Towers: '+ Reactor_Towers;
+  document.getElementById("Cluster").innerHTML = 'Reactors: '+ Reactor_Clusters;
   
 console.log(Reactor_Power)
 }
@@ -49,6 +53,7 @@ document.getElementById("Status-Cell").innerHTML = 'Power Cells:'+ Power_Cells;
 document.getElementById("Status-Heat").innerHTML = 'Heat: '+ Reactor_Heat/10 + ' C';
 document.getElementById("Price").innerHTML = 'Price for next cell: $'+ 50*Power_Cells;
 document.getElementById("Tower").innerHTML = 'Reactor Towers: '+ Reactor_Towers;
+  document.getElementById("Cluster").innerHTML = 'Reactors: '+ Reactor_Clusters;
   
 console.log(Reactor_Power)
 }
@@ -64,6 +69,7 @@ document.getElementById("Status-Cell").innerHTML = 'Power Cells:'+ Power_Cells;
 document.getElementById("Status-Heat").innerHTML = 'Heat: '+ Reactor_Heat/10 + ' C';
 document.getElementById("Price").innerHTML = 'Price for next cell: $'+ 50*Power_Cells;
 document.getElementById("Tower").innerHTML = 'Reactor Towers: '+ Reactor_Towers;
+  document.getElementById("Cluster").innerHTML = 'Reactors: '+ Reactor_Clusters;
 }
 cell = function cell(){
 
@@ -77,6 +83,7 @@ document.getElementById("Status-Cell").innerHTML = 'Power Cells:'+ Power_Cells;
 document.getElementById("Status-Heat").innerHTML = 'Heat: '+ Reactor_Heat/10 + ' C';
 document.getElementById("Price").innerHTML = 'Price for next cell: $'+ 50*Power_Cells;
 document.getElementById("Tower").innerHTML = 'Reactor Towers: '+ Reactor_Towers;
+  document.getElementById("Cluster").innerHTML = 'Reactors: '+ Reactor_Clusters;
 }
 tower = function tower(){
   if(Power_Cells > 14){
@@ -89,9 +96,23 @@ document.getElementById("Status-Cell").innerHTML = 'Power Cells:'+ Power_Cells;
 document.getElementById("Status-Heat").innerHTML = 'Heat: '+ Reactor_Heat/10 + ' C';
 document.getElementById("Price").innerHTML = 'Price for next cell: $'+ 50*Power_Cells;
 document.getElementById("Tower").innerHTML = 'Reactor Towers: '+ Reactor_Towers;
+  document.getElementById("Cluster").innerHTML = 'Reactors: '+ Reactor_Clusters;
 
 }
 
+cluster = function cluster(){
+  if(Reactor_Towers > 99){
+  Reactor_Clusters = Reactor_Clusters + 1
+  Reactor_Towers = Reactor_Towers - 100
+  }
+document.getElementById("Money").innerHTML = 'Money: $'+ Money;
+document.getElementById("Status").innerHTML = 'Power:'+ Reactor_Power;
+document.getElementById("Status-Cell").innerHTML = 'Power Cells:'+ Power_Cells;
+document.getElementById("Status-Heat").innerHTML = 'Heat: '+ Reactor_Heat/10 + ' C';
+document.getElementById("Price").innerHTML = 'Price for next cell: $'+ 50*Power_Cells;
+document.getElementById("Tower").innerHTML = 'Reactor Towers: '+ Reactor_Towers;
+document.getElementById("Cluster").innerHTML = 'Reactors: '+ Reactor_Clusters;
+}
 function addheat(x){
 heat = heat + x
 
